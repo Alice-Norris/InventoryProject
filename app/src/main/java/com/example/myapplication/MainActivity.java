03a8215b1386a3df5848b351a0a5d9f25d87d756
Alice Kneller <alice.norris@snhu.edu>
2022-03-28 20:52:07 -0400

    saving work

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,2 +1,20 @@
-package com.example.myapplication;public class MainActivity {
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Boolean authenticated = false;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!authenticated){
+            Intent loginIntent = new Intent(this, Welcome.class);
+            startActivity(loginIntent);
+        }
+    }
 }
